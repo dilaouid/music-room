7b264f819d30b53552a930f0d0ea673d2719dba1
Diyaeddine LAOUID <yedine@live.fr>
2021-01-17 15:06:47 +0100

    initial commit - login page front-end / templating back-end

---
+++ frontt-end/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthPage } from './auth.page';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthPage,
+    children: [
+      {
+        path: 'login',
+        loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/auth/login',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthPageRoutingModule {}

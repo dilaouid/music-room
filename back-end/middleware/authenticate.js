ac87269fe9f1c2995047008db2273c57952cf72b
Diyaeddine LAOUID <yedine@live.fr>
2021-01-18 14:16:16 +0100

    login complete / have to fix some issues and res

--- back-end/middleware/authenticate.js
+++ back-end/middleware/authenticate.js
@@ -1,9 +1,57 @@
 const express       = require('express');
 const router        = express.Router();
+const jwt           = require('jsonwebtoken');
 const cookieParser  = require('cookie-parser');
+const Validator     = require("validator");
+const bcrypt        = require("bcryptjs");
+const sanitize      = require('mongo-sanitize');
+
+/* ############# FUNCTIONS ############# */
+const authentified = (req, res, next) => {
+    let token;
+    if (req.params.token && !req.cookies.token) {
+        token = req.params.token;
+    } else {
+        token = req.cookies.token;
+    }
+    if (!token) {
+        res.status(400).send('Forbidden access: No token provided');
+    } else {
+        jwt.verify(token, process.env.SECRET, async function(err, decoded) {
+            if (err) {
+                res.status(400).send('Forbidden access: Provided token is invalid');
+            } else {
+                res.locals.id = decoded.id;
+                next();
+            }
+        });
+    }
+};
+
+/* ############# FUNCTIONS ############# */
 
 router.post('/login', (req, res) => {
-    /* ###### LOGIN ACTION HERE ###### */
+    const username = req.body.username;
+    var password = req.body.password;
+
+    User.findOne( {username} ).then(async(user) => {
+        if (!user || user && !user.password)
+            return res.status(400).json({});
+        bcrypt.compare(password, user.password).then(match => {
+            if (match) {
+                const payload = {id: user.id};
+                jwt.sign(payload, keys.secretKey, {expiresIn: 31556926}, (err, token) => {
+                    if (!err) {
+                        res.cookie('token', token, { maxAge: 2 * 60 * 60 * 1000, domain: 'localhost', secure: false, sameSite: true, httpOnly: false });
+                        return resizeTo.status(200).json({});
+                    }
+                    console.log(err);
+                    return res.status(400).json({});
+                })
+            } else
+                return res.status(400).json({});
+        });
+    });
 });
 
 router.post('/register', (req, res) => {
@@ -20,6 +68,11 @@ router.post('/check-code', (req, res) => {
 
 router.post('/new-password', (req, res) => {
     /* ###### NEW PASSWORD ACTION HERE ###### */
+});
+
+router.get ('/logout', authentified, (req, res) => {
+    res.clearCookie('token')
+    .sendStatus(200);
 });
 
 module.exports = router;

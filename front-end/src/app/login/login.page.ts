5256fafd80cae2e44072bccabb2f5fe4e91bb2e4
Diyaeddine LAOUID <yedine@live.fr>
2021-01-18 17:16:55 +0100

    add login

--- front-end/src/app/login/login.page.ts
+++ front-end/src/app/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService }      from 'ngx-cookie-service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment }        from './../../environments/environment';
+import axios from 'axios';
+
+declare var $: any;
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  private cookie: string;
+  constructor(private router: Router, private route: ActivatedRoute, private cookieService: CookieService) { }
 
   ngOnInit() {
+  }
+
+  inputLog(login:string, password:string)
+  {
+    if (login.length > 3 && password.length > 5) {
+      $('#loginbtn').removeAttr('disabled');
+    } else {
+      $('#loginbtn').attr('disabled', 'disabled');
+    }
+  }
+
+  validEmail(email:string):Boolean {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  private async login(login:string, password:string):Promise<void> {
+    $('#loginbtn').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>');
+    var ret;
+    var msg;
+    if (login.trim().length < 3 || password.trim().length < 3) {
+      $('#alert').removeClass('d-none');
+      $('#alertmsg').html('Invalid fields!');
+      $('#loginbtn').html('Log In');
+    } else {
+      try {
+        await axios.post(`${environment.backEndUrl}/login`, {withCredentials: true})
+              .then(res => {
+                  ret = res.data.statut;
+                  msg = res.data.msg;
+                  if (this.cookieService.get('10HUser')) {
+                    this.cookie = this.cookieService.get('EnigmaPlayer');
+                  }
+              });
+      }
+      catch (err) {
+        console.error(err);
+      }
+      if (ret != 200) {
+        $('#alert').removeClass('d-none');
+        $('#alertmsg').html("The password or the username is incorrect!");
+        $('#loginbtn').html('Log In');
+      } else {
+        console.log('success login');
+      }
+    }
+
+
+  }
+
+  goToRegister() {
+    this.router.navigateByUrl('/register');
   }
 
 }

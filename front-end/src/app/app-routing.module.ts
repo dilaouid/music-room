0dbef9edd599ffc95f4497503cbd0b88da82d611
Diyaeddine LAOUID <yedine@live.fr>
2021-02-09 22:18:41 +0100

    fix routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -23,26 +23,6 @@ const routes: Routes = [
     loadChildren: () => import('./validate-account/validate-account.module').then( m => m.ValidateAccountPageModule)
   },
   {
-    path: 'edit-profile',
-    loadChildren: () => import('./edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
-  },
-  {
-    path: 'edit-playlist',
-    loadChildren: () => import('./edit-playlist/edit-playlist.module').then( m => m.EditPlaylistPageModule)
-  },
-  {
-    path: 'edit-event',
-    loadChildren: () => import('./edit-event/edit-event.module').then( m => m.EditEventPageModule)
-  },
-  {
-    path: 'new-event',
-    loadChildren: () => import('./new-event/new-event.module').then( m => m.NewEventPageModule)
-  },
-  {
-    path: 'new-playlist',
-    loadChildren: () => import('./new-playlist/new-playlist.module').then( m => m.NewPlaylistPageModule)
-  },
-  {
     path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
